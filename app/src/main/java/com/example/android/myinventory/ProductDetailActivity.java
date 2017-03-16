2d102ed7df434b6f7ffe7ec8bf411658aa8264b5
lixiaochi <lmt199027@gmail.com>
2017-03-16 13:43:08 +1000

    allows users to delete one product.

--- app/src/main/java/com/example/android/myinventory/ProductDetailActivity.java
+++ app/src/main/java/com/example/android/myinventory/ProductDetailActivity.java
@@ -1,16 +1,22 @@
 package com.example.android.myinventory;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by lixiaochi on 15/3/17.
@@ -21,7 +27,7 @@ public class ProductDetailActivity extends AppCompatActivity implements android.
     /**
      * Content URI for the existing pet (null if it's a new pet)
      */
-    private Uri mCurrentPetUri;
+    private Uri mCurrentProductUri;
 
     private static final int EXISTING_PRODUCT_LOADER = 0;
 
@@ -37,6 +43,8 @@ public class ProductDetailActivity extends AppCompatActivity implements android.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales_details);
         setTitle("Product Details");
+        Intent intent = getIntent();
+        mCurrentProductUri = intent.getData();
 
         getLoaderManager().initLoader(EXISTING_PRODUCT_LOADER, null, this);
 
@@ -49,8 +57,23 @@ public class ProductDetailActivity extends AppCompatActivity implements android.
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_product_details, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.action_delete:
+                showDeleteConfirmationDialog();
+                return true;
+            case R.id.action_receive:
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(ProductDetailActivity.this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -66,5 +89,54 @@ public class ProductDetailActivity extends AppCompatActivity implements android.
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
 
+    }
+
+
+    private void showDeleteConfirmationDialog() {
+        // Create an AlertDialog.Builder and set the message, and click listeners
+        // for the postivie and negative buttons on the dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Want to delete");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Delete" button, so delete the pet.
+                deleteProduct();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Cancel" button, so dismiss the dialog
+                // and continue editing the pet.
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void deleteProduct() {
+        // Only perform the delete if this is an existing pet.
+        if (mCurrentProductUri != null) {
+            // Call the ContentResolver to delete the pet at the given content URI.
+            // Pass in null for the selection and selection args because the mCurrentProductUri
+            // content URI already identifies the pet that we want.
+            int rowsDeleted = getContentResolver().delete(mCurrentProductUri, null, null);
+
+            // Show a toast message depending on whether or not the delete was successful.
+            if (rowsDeleted == 0) {
+                // If no rows were deleted, then there was an error with the delete.
+                Toast.makeText(this, "delete fail",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                // Otherwise, the delete was successful and we can display a toast.
+                Toast.makeText(this, "delete success",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+        finish();
     }
 }
